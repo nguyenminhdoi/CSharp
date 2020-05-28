c893631b1e04ce715afe2d2fcb199b4805f0937f
nguyenminhdoi <66059484+nguyenminhdoi@users.noreply.github.com>
2020-05-28 16:11:55 +0700

    Add files via upload

---
+++ F_CapNhatLoaiSach.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace quanLyThuVien
+{
+    public partial class F_CapNhatLoaiSach : Form
+    {
+        public F_CapNhatLoaiSach()
+        {
+            InitializeComponent();
+        }
+
+        private void F_CapNhatLoaiSach_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
